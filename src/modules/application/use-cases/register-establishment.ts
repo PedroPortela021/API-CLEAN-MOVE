f71e35e1629dc54a63fdb1860fe6a2cfaa7cb0d1
Felipe Pereira <fehpereira237@gmail.com>
2026-03-19 13:12:39 -0300

    feat: create `resource-already-exists-error` and implement `register-establishment` use-case with business rules

---
+++ src/modules/application/use-cases/register-establishment.ts
@@ -0,0 +1,91 @@
+import { Either, left, right } from "../../../shared/either";
+import { ResourceAlreadyExistsError } from "../../../shared/errors/resource-already-exists-error";
+import { User } from "../../accounts/domain/entities/user";
+import { Address } from "../../accounts/domain/value-objects/address";
+import { Email } from "../../accounts/domain/value-objects/email";
+import { UserRole } from "../../accounts/domain/value-objects/user-role";
+import { Establishment } from "../../establishments/domain/entities/establishment";
+import { Cnpj } from "../../establishments/domain/value-objects/cnpj";
+import { EstablishmentsRepository } from "../repositories/establishment-repository";
+import { HashGenerator } from "../repositories/hash-generator";
+import { UsersRepository } from "../repositories/users-repository";
+
+type RegisterEstablishmentUseCaseRequest = {
+  name: string;
+  corporateName: string;
+  socialReason: string;
+  email: Email;
+  password: string;
+  cnpj: Cnpj;
+  phone: string;
+  address: Address;
+};
+
+type RegisterEstablishmentUseCaseResponse = Either<
+  ResourceAlreadyExistsError,
+  {
+    establishment: Establishment;
+  }
+>;
+
+export class RegisterEstablishmentUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private establishmentsRepository: EstablishmentsRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    corporateName,
+    socialReason,
+    email,
+    password,
+    cnpj,
+    phone,
+    address,
+  }: RegisterEstablishmentUseCaseRequest): Promise<RegisterEstablishmentUseCaseResponse> {
+    const [establishmentWithTheSameCnpj, userWithTheSameEmail] =
+      await Promise.all([
+        this.establishmentsRepository.findByCnpj(cnpj.toString()),
+        this.usersRepository.findByEmail(email.toString()),
+      ]);
+
+    if (establishmentWithTheSameCnpj || userWithTheSameEmail) {
+      return left(
+        new ResourceAlreadyExistsError("Establishment already registered."),
+      );
+    }
+
+    const userRole: UserRole = "ESTABLISHMENT";
+
+    const hashedPassword = await this.hashGenerator.hash(password);
+
+    const userInputValues = {
+      name,
+      email,
+      hashedPassword,
+      role: userRole,
+      phone,
+      address,
+    };
+
+    const user = User.create(userInputValues);
+
+    const establishment = Establishment.create({
+      ownerId: user.id,
+      cnpj,
+      corporateName,
+      socialReason,
+    });
+
+    await Promise.all([
+      this.usersRepository.create(user),
+      this.establishmentsRepository.create(establishment),
+    ]);
+
+    return right({
+      establishment,
+    });
+  }
+}
