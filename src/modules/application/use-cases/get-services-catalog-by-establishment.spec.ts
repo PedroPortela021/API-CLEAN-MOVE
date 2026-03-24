bc6b0fcd5b790fa4b171601f709f0b228a81cebb
Felipe Pereira <fehpereira237@gmail.com>
2026-03-24 10:22:03 -0300

    test: test coverage for the `get-services-catalog-by-establishment` use case

---
+++ src/modules/application/use-cases/get-services-catalog-by-establishment.spec.ts
@@ -0,0 +1,227 @@
+import { Money } from "../../catalog/domain/value-objects/money";
+import { ResourceNotFoundError } from "../../../shared/errors/resource-not-found-error";
+import { makeEstablishment } from "../../../tests/factories/establishment-factory";
+import { makeService } from "../../../tests/factories/service-factory";
+import { InMemoryEstablishmentsRepository } from "../../../tests/repositories/in-memory-establishment-repository";
+import { InMemoryServicesRepository } from "../../../tests/repositories/in-memory-services-repository";
+import { getFirstItem } from "../../../tests/utils/get-first-item";
+import { GetServiceCatalogByEstablishmentUseCase } from "./get-services-catalog-by-establishment";
+
+let inMemoryServicesRepository: InMemoryServicesRepository;
+let inMemoryEstablishmentsRepository: InMemoryEstablishmentsRepository;
+
+let sut: GetServiceCatalogByEstablishmentUseCase;
+
+describe("Get services", () => {
+  beforeEach(() => {
+    inMemoryServicesRepository = new InMemoryServicesRepository();
+    inMemoryEstablishmentsRepository = new InMemoryEstablishmentsRepository();
+
+    sut = new GetServiceCatalogByEstablishmentUseCase(
+      inMemoryServicesRepository,
+      inMemoryEstablishmentsRepository,
+    );
+  });
+
+  it("should be able to get many services of an unique establishment", async () => {
+    const establishment = makeEstablishment();
+
+    await inMemoryEstablishmentsRepository.create(establishment);
+
+    for (let i = 0; i < 23; i++) {
+      await inMemoryServicesRepository.create(
+        makeService({
+          establishmentId: establishment.id,
+        }),
+      );
+    }
+
+    const result = await sut.execute({
+      establishmentId: establishment.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+
+    if (result.isLeft()) {
+      throw result.value;
+    }
+
+    expect(inMemoryServicesRepository.items).toHaveLength(23);
+    expect(result.value.services).toHaveLength(20);
+
+    const result2 = await sut.execute({
+      establishmentId: establishment.id.toString(),
+      filters: { page: 2 },
+    });
+
+    expect(result2.isRight()).toBe(true);
+
+    if (result2.isLeft()) {
+      throw result2.value;
+    }
+
+    expect(result2.value.services).toHaveLength(3);
+  });
+
+  it("should be able to get only services from the requested establishment", async () => {
+    const establishment = makeEstablishment();
+    const anotherEstablishment = makeEstablishment();
+
+    await inMemoryEstablishmentsRepository.create(establishment);
+    await inMemoryEstablishmentsRepository.create(anotherEstablishment);
+
+    await inMemoryServicesRepository.create(
+      makeService({
+        serviceName: "Lavagem simples",
+        establishmentId: establishment.id,
+      }),
+    );
+
+    await inMemoryServicesRepository.create(
+      makeService({
+        serviceName: "Higienizacao interna",
+        establishmentId: anotherEstablishment.id,
+      }),
+    );
+
+    const result = await sut.execute({
+      establishmentId: establishment.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+
+    if (result.isLeft()) {
+      throw result.value;
+    }
+
+    expect(result.value.services).toHaveLength(1);
+    const firstService = getFirstItem(result.value.services);
+
+    expect(firstService.establishmentId.toString()).toBe(
+      establishment.id.toString(),
+    );
+    expect(firstService.serviceName).toBe("Lavagem simples");
+  });
+
+  it("should be able to get many services of an unique establishment with filters", async () => {
+    const establishment = makeEstablishment();
+
+    await inMemoryEstablishmentsRepository.create(establishment);
+
+    await inMemoryServicesRepository.create(
+      makeService({
+        serviceName: "service-0",
+        category: "WASH",
+        price: Money.create(15000),
+        establishmentId: establishment.id,
+      }),
+    );
+
+    await inMemoryServicesRepository.create(
+      makeService({
+        serviceName: "service-1",
+        category: "WASH",
+        price: Money.create(25000),
+        establishmentId: establishment.id,
+      }),
+    );
+
+    await inMemoryServicesRepository.create(
+      makeService({
+        serviceName: "service-2",
+        category: "PROTECTION",
+        price: Money.create(35000),
+        establishmentId: establishment.id,
+      }),
+    );
+
+    for (let i = 3; i < 23; i++) {
+      await inMemoryServicesRepository.create(
+        makeService({
+          serviceName: `service-${i}`,
+          category: "WASH",
+          price: Money.create(30000),
+          establishmentId: establishment.id,
+        }),
+      );
+    }
+
+    const result = await sut.execute({
+      establishmentId: establishment.id.toString(),
+      filters: {
+        category: "WASH",
+        minPrice: 20000,
+        serviceName: "service-1",
+      },
+    });
+
+    expect(result.isRight()).toBe(true);
+
+    if (result.isLeft()) {
+      throw result.value;
+    }
+
+    expect(result.value.services).toHaveLength(1);
+    const filteredService = getFirstItem(result.value.services);
+
+    expect(filteredService.serviceName).toBe("service-1");
+
+    const result2 = await sut.execute({
+      establishmentId: establishment.id.toString(),
+      filters: {
+        category: "WASH",
+        minPrice: 20000,
+      },
+    });
+
+    expect(result2.isRight()).toBe(true);
+
+    if (result2.isLeft()) {
+      throw result2.value;
+    }
+
+    expect(result2.value.services).toHaveLength(20);
+
+    const result3 = await sut.execute({
+      establishmentId: establishment.id.toString(),
+      filters: {
+        minPrice: 40000,
+      },
+    });
+
+    expect(result3.isRight()).toBe(true);
+
+    if (result3.isLeft()) {
+      throw result3.value;
+    }
+
+    expect(result3.value.services).toHaveLength(0);
+
+    const result4 = await sut.execute({
+      establishmentId: establishment.id.toString(),
+      filters: {
+        maxPrice: 20000,
+      },
+    });
+
+    expect(result4.isRight()).toBe(true);
+
+    if (result4.isLeft()) {
+      throw result4.value;
+    }
+
+    expect(result4.value.services).toHaveLength(1);
+    const cheaperService = getFirstItem(result4.value.services);
+
+    expect(cheaperService.serviceName).toBe("service-0");
+  });
+
+  it("should not be able to get services from a non-existent establishment", async () => {
+    const result = await sut.execute({
+      establishmentId: "non-existent-establishment",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+  });
+});
