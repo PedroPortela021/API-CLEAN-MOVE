d9c3acdcdad6998a459c238a3391a30b8ad8ec91
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-03-24 10:59:38 -0300

    feat: implement GetCustomer use case with error handling and unit tests for customer retrieval by CPF

---
+++ src/modules/application/use-cases/customer/get-customer.ts
@@ -0,0 +1,34 @@
+import { Cpf } from "../../../accounts/domain/value-objects/cpf";
+import { Customer } from "../../../customer/domain/entities/customer";
+import { Either, left, right } from "../../../../shared/either";
+import { ResourceNotFoundError } from "../../../../shared/errors/resource-not-found-error";
+import { CustomersRepository } from "../../repositories/customers-repository";
+
+type GetCustomerUseCaseRequest = {
+  cpf: Cpf;
+};
+
+type GetCustomerUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    customer: Customer;
+  }
+>;
+
+export class GetCustomerUseCase {
+  constructor(private customersRepository: CustomersRepository) {}
+
+  async execute({
+    cpf,
+  }: GetCustomerUseCaseRequest): Promise<GetCustomerUseCaseResponse> {
+    const customer = await this.customersRepository.findByCpf(cpf.toString());
+
+    if (!customer) {
+      return left(new ResourceNotFoundError("Customer not found."));
+    }
+
+    return right({
+      customer,
+    });
+  }
+}
