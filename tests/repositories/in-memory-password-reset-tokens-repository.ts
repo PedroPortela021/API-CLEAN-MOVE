afdab47ca8308f3b37e5a212b5f31cebb466688c
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-04-02 14:53:19 -0300

    test: add InMemoryPasswordResetTokensRepository

---
+++ tests/repositories/in-memory-password-reset-tokens-repository.ts
@@ -0,0 +1,22 @@
+import { PasswordResetToken } from "../../src/modules/accounts/domain/entities/password-reset-token";
+import { PasswordResetTokensRepository } from "../../src/modules/application/repositories/password-reset-tokens-repository";
+
+export class InMemoryPasswordResetTokensRepository implements PasswordResetTokensRepository {
+  public items: PasswordResetToken[] = [];
+
+  async upsert(token: PasswordResetToken): Promise<void> {
+    const userId = token.userId.toString();
+    this.items = this.items.filter((t) => t.userId.toString() !== userId);
+    this.items.push(token);
+  }
+
+  async findByUserId(userId: string): Promise<PasswordResetToken | null> {
+    const token = this.items.find((t) => t.userId.toString() === userId);
+
+    return token ?? null;
+  }
+
+  async deleteByUserId(userId: string): Promise<void> {
+    this.items = this.items.filter((t) => t.userId.toString() !== userId);
+  }
+}
