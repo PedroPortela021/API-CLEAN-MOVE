8df44f0a424eecb575c48f7b44e84ffb22d81356
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-04-02 13:59:06 -0300

    feat: add findByProviderAndSubject to UsersRepository

--- src/modules/application/repositories/users-repository.ts
+++ src/modules/application/repositories/users-repository.ts
@@ -1,8 +1,13 @@
+import type { OAuthProvider } from "../../accounts/domain/value-objects/oauth-provider";
 import { User } from "../../accounts/domain/entities/user";
 
 export abstract class UsersRepository {
   abstract create(user: User): Promise<void>;
   abstract findByEmail(email: string): Promise<User | null>;
   abstract findById(userId: string): Promise<User | null>;
+  abstract findByProviderAndSubject(
+    provider: OAuthProvider,
+    subjectId: string,
+  ): Promise<User | null>;
   abstract save(user: User): Promise<void>;
 }
