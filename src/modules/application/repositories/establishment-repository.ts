eaa8614517096852c920c07162482f9f58b1bb54
Felipe Pereira <fehpereira237@gmail.com>
2026-04-02 14:10:20 -0300

    test: adjust repository contracts and in-memory establishment listing

--- src/modules/application/repositories/establishment-repository.ts
+++ src/modules/application/repositories/establishment-repository.ts
@@ -1,6 +1,10 @@
+import { ServiceCategory } from "../../catalog/domain/value-objects/service-category";
 import { Establishment } from "../../establishments/domain/entities/establishment";
+import { ServicesRepository } from "./services-repository";
 
 export abstract class EstablishmentsRepository {
+  constructor(servicesRepository: ServicesRepository) {}
+
   abstract create(data: Establishment): Promise<void>;
   abstract findById(id: string): Promise<Establishment | null>;
   abstract findByCnpj(cnpj: string): Promise<Establishment | null>;
@@ -9,4 +13,8 @@ export abstract class EstablishmentsRepository {
     cnpj: string,
     slug: string,
   ): Promise<Establishment | null>;
+  abstract findMany(filters?: {
+    establishmentName?: string;
+    serviceCategory?: ServiceCategory;
+  }): Promise<Establishment[]>;
 }
