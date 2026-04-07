4d5850e9f7dcc4f80777b8dd51571b79151b6332
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-04-07 11:58:28 -0300

    refactor: remove service category from booked snapshot

--- src/modules/application/use-cases/appointment/book-service.ts
+++ src/modules/application/use-cases/appointment/book-service.ts
@@ -150,7 +150,6 @@ export class BookServiceUseCase {
         customerId: new UniqueEntityId(customerId),
         establishmentId: new UniqueEntityId(establishmentId),
         service: BookedServiceSnapshot.create({
-          category: service.category,
           durationInMinutes: service.estimatedDuration.maxInMinutes,
           priceInCents: service.price.amountInCents,
           serviceId: service.id,
