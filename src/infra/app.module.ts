414500525ee2e6caf64608f1121f258728326ff1
Felipe Pereira <fehpereira237@gmail.com>
2026-04-09 12:02:20 -0300

    feat: add initial structure for establishment-related components and mappers

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { DatabaseModule } from "./database/database.module";
 import { EnvModule } from "./env/env.module";
+import { HttpModule } from "./http/http.module";
 
 @Module({
-  imports: [EnvModule, DatabaseModule],
-  controllers: [],
+  imports: [EnvModule, DatabaseModule, HttpModule],
   providers: [],
 })
 export class AppModule {}
