6faf4e7f46278e70613d47a3ec420bfdcd59c19f
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-04-16 15:44:38 -0300

    feat: register OAuth verifier and token service providers

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -2,13 +2,19 @@ import { Module } from "@nestjs/common";
 import { HashGenerator } from "../../modules/application/repositories/hash-generator";
 import { BcryptHasher } from "./bcrypt-hasher";
 import { HashComparer } from "../../modules/application/repositories/hash-comparer";
+import { OAuthIdTokenVerifier } from "../../modules/application/services/oauth-id-token-verifier";
+import { GoogleIdTokenVerifier } from "./google-id-token-verifier";
+import { AuthTokenService } from "../../modules/application/services/auth-token-service";
+import { JwtTokenService } from "./jwt-token.service";
 
 @Module({
   providers: [
     BcryptHasher,
     { provide: HashGenerator, useExisting: BcryptHasher },
     { provide: HashComparer, useExisting: BcryptHasher },
+    { provide: OAuthIdTokenVerifier, useClass: GoogleIdTokenVerifier },
+    { provide: AuthTokenService, useClass: JwtTokenService },
   ],
-  exports: [HashGenerator, HashComparer],
+  exports: [HashGenerator, HashComparer, OAuthIdTokenVerifier, AuthTokenService],
 })
 export class AuthModule {}
