b35456e05033a7f595e0e7f08ee3ebe1f77654dc
Felipe Pereira <fehpereira237@gmail.com>
2026-04-16 17:51:50 -0300

    fix: merge conflict

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,30 +1,26 @@
 import { Module } from "@nestjs/common";
-
-import { RegisterCustomerUseCase } from "../../modules/application/use-cases/customer/register-customer";
-import { RegisterCustomerController } from "./controllers/register-customer.controller";
 import { RegisterEstablishmentUseCase } from "../../modules/application/use-cases/establishment/register-establishment";
+import { SessionCreationService } from "../../modules/accounts/domain/services/session-creation-service";
+import { LoginWithCredentialsController } from "./controllers/login-with-credentials.controller";
 import { RegisterEstablishmentController } from "./controllers/register-establishment.controller";
 import { AuthModule } from "../auth/auth.module";
-import { LoginWithCredentialsController } from "./controllers/login-with-credentials.controller";
-import { LoginWithCredentialsUseCase } from "../../modules/application/use-cases/auth/login-with-credentials";
 import { DatabaseModule } from "../database/database.module";
-import { SessionCreationService } from "../../modules/accounts/domain/services/session-creation-service";
+import { LoginWithCredentialsUseCase } from "../../modules/application/use-cases/auth/login-with-credentials";
+import { RegisterCustomerController } from "./controllers/register-customer.controller";
+import { RegisterCustomerUseCase } from "../../modules/application/use-cases/customer/register-customer";
 
 @Module({
   imports: [AuthModule, DatabaseModule],
   controllers: [
     RegisterEstablishmentController,
+    RegisterCustomerController,
     LoginWithCredentialsController,
   ],
   providers: [
     RegisterEstablishmentUseCase,
     LoginWithCredentialsUseCase,
+    RegisterCustomerUseCase,
     SessionCreationService,
   ],
-
-@Module({
-  imports: [AuthModule],
-  controllers: [RegisterEstablishmentController, RegisterCustomerController],
-  providers: [RegisterEstablishmentUseCase, RegisterCustomerUseCase],
 })
 export class HttpModule {}
