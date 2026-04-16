eb5cc8b7dc0696a91d08f72862e7ddcfb52b5b00
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-04-16 15:44:35 -0300

    feat: wire Google OAuth controller and use case

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,13 +2,23 @@ import { Module } from "@nestjs/common";
 
 import { RegisterCustomerUseCase } from "../../modules/application/use-cases/customer/register-customer";
 import { RegisterEstablishmentUseCase } from "../../modules/application/use-cases/establishment/register-establishment";
+import { AuthenticateWithOAuthUseCase } from "../../modules/application/use-cases/auth/authenticate-with-oauth";
 import { AuthModule } from "../auth/auth.module";
+import { AuthenticateWithGoogleController } from "./controllers/authenticate-with-google.controller";
 import { RegisterCustomerController } from "./controllers/register-customer.controller";
 import { RegisterEstablishmentController } from "./controllers/register-establishment.controller";
 
 @Module({
   imports: [AuthModule],
-  controllers: [RegisterEstablishmentController, RegisterCustomerController],
-  providers: [RegisterEstablishmentUseCase, RegisterCustomerUseCase],
+  controllers: [
+    RegisterEstablishmentController,
+    RegisterCustomerController,
+    AuthenticateWithGoogleController,
+  ],
+  providers: [
+    RegisterEstablishmentUseCase,
+    RegisterCustomerUseCase,
+    AuthenticateWithOAuthUseCase,
+  ],
 })
 export class HttpModule {}
