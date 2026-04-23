01a6ce1c8beac730404a85e1ad38147ed92eea8a
Felipe Pereira <fehpereira237@gmail.com>
2026-04-22 21:04:32 -0300

    feat: integrate Swagger for API documentation

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 import { EnvService } from "./env/env.service";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,20 @@ async function bootstrap(): Promise<void> {
     credentials: true,
   });
 
-  await app.listen(port, "0.0.0.0");
+  const config = new DocumentBuilder()
+    .setTitle("API Clean Move")
+    .setDescription("API documentation")
+    .setVersion("1.0")
+    .addTag("auth", "Authentication and session management")
+    .addTag("register", "Customer and establishment registration")
+    .addTag("appointment", "Appointment management")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
+  await app.listen(port, "0.0.0.0", () =>
+    console.log("Swagger: http://localhost:3000/api"),
+  );
 }
 
 void bootstrap();
